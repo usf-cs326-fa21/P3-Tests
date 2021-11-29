8e7bbd32464fc857135a1c1d23ae3d7132a01ba7
Matthew Malensek <matt@malensek.net>
2021-11-29 14:40:42 -0800

    Update test progs

--- progs/allocations-1.c
+++ progs/allocations-1.c
@@ -3,22 +3,22 @@
 
 int main(void)
 {
-    void *a = malloc_name(100, "Test Allocation: 0");
-    void *b = malloc_name(100, "Test Allocation: 1"); /* Will be deleted */
-    void *c = malloc_name(100, "Test Allocation: 2");
-    void *d = malloc_name(10,  "Test Allocation: 3");  /* Will be deleted */
-    void *e = malloc_name(100, "Test Allocation: 4");
-    void *f = malloc_name(100, "Test Allocation: 5");
+    void *a = malloc_impl(100, "Test Allocation: 0");
+    void *b = malloc_impl(10,  "Test Allocation: 1"); /* Will be deleted */
+    void *c = malloc_impl(100, "Test Allocation: 2");
+    void *d = malloc_impl(100, "Test Allocation: 3"); /* Will be deleted */
+    void *e = malloc_impl(100, "Test Allocation: 4");
+    void *f = malloc_impl(100, "Test Allocation: 5");
 
-    free(b);
-    free(d);
+    free_impl(b);
+    free_impl(d);
 
     /* This will split:
-     * - b with first fit
-     * - d with best fit
+     * - d with first fit (most recently freed)
+     * - b with best fit
      * - f with worst fit
      */
-    void *g = malloc_name(10, "Test Allocation: 6");
+    void *g = malloc_impl(10, "Test Allocation: 6");
 
     print_memory();
 
--- progs/allocations-3.c
+++ progs/allocations-3.c
@@ -3,18 +3,18 @@
 
 int main(void)
 {
-    void *a = malloc_name(500, "Test Allocation: 0");
-    void *b = malloc_name(1000, "Test Allocation: 1");
-    void *c = malloc_name(250, "Test Allocation: 2");
-    void *d = malloc_name(294, "Test Allocation: 3");
-    void *e = malloc_name(400, "Test Allocation: 4");
+    void *a = malloc_impl(500, "Test Allocation: 0");
+    void *b = malloc_impl(294, "Test Allocation: 1");
+    void *c = malloc_impl(250, "Test Allocation: 2");
+    void *d = malloc_impl(1000, "Test Allocation: 3");
+    void *e = malloc_impl(400, "Test Allocation: 4");
 
-    free(b);
-    free(d);
+    free_impl(b);
+    free_impl(d);
 
-    void *f = malloc_name(600, "Test Allocation: 5");
-    void *g = malloc_name(150, "Test Allocation: 6");
-    void *h = malloc_name(44, "Test Allocation: 7");
+    void *f = malloc_impl(600, "Test Allocation: 5");
+    void *g = malloc_impl(150, "Test Allocation: 6");
+    void *h = malloc_impl(44, "Test Allocation: 7");
 
     print_memory();
 
