d4d6fa08cf40817bb0b94ed66be6c7b3380ade2d
Matthew Malensek <matt@malensek.net>
2021-11-29 11:10:15 -0800

    Disable symlink creation

--- prerun/00-symlink.bash
+++ prerun/00-symlink.bash
@@ -1,3 +1,6 @@
 
-ln -fs "${TEST_DIR}/../allocator.so" "${TEST_DIR}/../liballocator.so"
+# No longer used since "liballocator" and "allocator" are now two separate
+# entities for testing.
+
+#ln -fs "${TEST_DIR}/../allocator.so" "${TEST_DIR}/../liballocator.so"
 
