2da3f11ab1591503118d971b774508ab7b299fab
Matthew Malensek <matt@malensek.net>
2021-11-29 11:21:16 -0800

    Add makefile fix

---
+++ prerun/00-Makefile_check.bash
@@ -0,0 +1,11 @@
+if [[ ! -f "${TEST_DIR}/../liballocator.so" \
+        || -L "${TEST_DIR}/../liballocator.so" ]]; then
+    echo "Test library was not built correctly. This probably means you need "
+    echo -n "an updated makefile. Can I fix this for you automatically? [y/N] "
+    read response
+    if [[ "${response}" == "y" || "${response}" == "Y" ]]; then
+        cp -v "${TEST_DIR}/New-Makefile" "${TEST_DIR}/../Makefile"
+        echo "Please retry your tests now."
+        exit
+    fi
+fi
